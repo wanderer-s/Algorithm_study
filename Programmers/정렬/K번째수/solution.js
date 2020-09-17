b870ee944c178a2e6c09df234708e224f03ac5a3
Wanderer-s <ows0871@gmail.com>
2020-09-17 18:50:49 +0900

    add comments on k번째 수 solution

--- Programmers/정렬/K번째수/solution.js
+++ Programmers/정렬/K번째수/solution.js
@@ -2,13 +2,13 @@ function solution(array, commands) {
   var answer = [];
   
   for(let n = 0; n < commands.length; n++) {
-      let i = commands[n][0]
-      let j = commands[n][1]
-      let k = commands[n][2]
+      let i = commands[n][0] // commands의 첫번째 숫자 - 몇번째 부터
+      let j = commands[n][1] // commands의 두번째 숫자 - 몇번째 까지
+      let k = commands[n][2] // commands의 세번째 숫자 - 몇번째 숫자
       
-      let result = array.slice(i-1, j)
-      result.sort( (a,b) => a - b)
-      answer.push(result[k-1])
+      let result = array.slice(i-1, j) //  index는 0부터이니 i - 1
+      result.sort( (a,b) => a - b) // 정렬
+      answer.push(result[k-1]) // 마찬가지로 index는 0부터라서 k - 1
   }
   return answer;
 }
