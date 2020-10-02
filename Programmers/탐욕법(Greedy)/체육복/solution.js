a321318f148bf6c3529267fafcda3772a20d63f1
wanderer-s <ows0871@gmail.com>
2020-10-02 19:17:29 +0900

    add comments in 체육복 solution

--- Programmers/탐욕법(Greedy)/체육복/solution.js
+++ Programmers/탐욕법(Greedy)/체육복/solution.js
@@ -15,19 +15,20 @@ function solution(n, lost, reserve) {
         }
     }
     for(let i = 0; i < n; i++) {
-        if(studentsCloths[i] === 0) {
-            if(studentsCloths[i - 1] === 2) {
-                studentsCloths[i - 1] --
+        if(studentsCloths[i] === 0) { // 체육복을 잃어버린 학생의 경우
+            if(studentsCloths[i - 1] === 2) { // 전 번호의 학생이 체육복이 2 개면
+                studentsCloths[i - 1] -- // 체육복을 현재번호의 학생에게 전달
                 studentsCloths[i] ++
             } else if(studentsCloths[i + 1] === 2) {
-                studentsCloths[i + 1] --
+                // 전 번호의 학생이 체육복이 2개가 아니면 다음 번호의 학생 체육복을 확인하여 2개면
+                studentsCloths[i + 1] -- // 체육복을 현재번호의 학생에게 전달
                 studentsCloths[i] ++
             }
         }
     }
     let students = 0
     for(let i = 0; i < n; i++) {
-        if(studentsCloths[i]) {
+        if(studentsCloths[i]) { // 체육복을 가지고 있는 학생수를 샌다 (studentsCloths가 0 이면 false)
             students ++
         }
     }
